9752b9cc9034dc07be54d80c07a36d49d5e31c43
Alireza Poodineh <pa789892@outlook.com>
2024-05-30 06:01:38 +0000

    fix wrong code
    Signed-off-by: Alireza Poodineh <pa789892@outlook.com>

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # firstly run these commands
-# apt install -y wget perl python3 cmake unzip git ninja-build g++ p7zip-full ca-certificates gnupg
+# sudo apt install -y wget perl python3 cmake unzip git ninja-build g++ p7zip-full ca-certificates gnupg
 
 # sudo gpg --homedir /tmp --no-default-keyring --keyring /usr/share/keyrings/mono-official-archive-keyring.gpg --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF
 # echo "deb [signed-by=/usr/share/keyrings/mono-official-archive-keyring.gpg] https://download.mono-project.com/repo/ubuntu stable-focal main" | sudo tee /etc/apt/sources.list.d/mono-official-stable.list
