ae8c33fde3d843a9d38e6b14b875d02625bc99eb
Alireza Poodineh <pa789892@outlook.com>
2024-05-30 05:57:30 +0000

    update setup script
    Signed-off-by: Alireza Poodineh <pa789892@outlook.com>

--- setup.sh
+++ setup.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 
 # firstly run these commands
-# apt install wget perl python3 cmake unzip git ninja-build g++ p7zip-full 
+# apt install -y wget perl python3 cmake unzip git ninja-build g++ p7zip-full ca-certificates gnupg
 
-# sudo apt install ca-certificates gnupg
 # sudo gpg --homedir /tmp --no-default-keyring --keyring /usr/share/keyrings/mono-official-archive-keyring.gpg --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF
 # echo "deb [signed-by=/usr/share/keyrings/mono-official-archive-keyring.gpg] https://download.mono-project.com/repo/ubuntu stable-focal main" | sudo tee /etc/apt/sources.list.d/mono-official-stable.list
 # sudo apt update
-# sudo apt install mono-devel
+# sudo apt install -y mono-devel
 
 mkdir -p .ndk
 mkdir -p .msbuild
